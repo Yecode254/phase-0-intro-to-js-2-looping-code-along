6394a083b40ed48fc9b078af94a0aca44adbd2e9
Yego <eddieyego273@gmail.com>
2025-03-13 23:13:22 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+const names = []
+
+function writeCards(names, event) {
+   return  names.map(name=> `Thank you, ${name}, for the wonderful ${event} gift!`)
+}
+
+console.log(writeCards(names, "birthday"))
+
+
+let n= "";
+function countDown(n) {
+    while( n >= 0) {
+        console.log(n);
+        n--;
+    }
+}
+countdown();
